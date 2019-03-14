0003af2df3a7ab28b90fe993ecb9d4f400081cf6
Nicolas Leduc <compwiz450@hotmail.com>
2019-03-14 08:55:11 -0600

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -25,6 +25,30 @@ $(document).ready(function () {
   });
 
 
+  // Element Animation in viewport
+  $(document).ready(function () {
+    // Check if element is scrolled into view
+    function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    // If element is scrolled into view, fade it in
+    $(window).scroll(function () {
+      $('.scroll-animations .animated').each(function () {
+        if (isScrolledIntoView(this) === true) {
+          $(this).addClass('fadeIn');
+          $(this).removeClass('hidden');
+        }
+      });
+    });
+  });
+
+
   // SCROLL BACK TO TOP BUTTON
   $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
